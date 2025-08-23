7fe8971cf8081e1c5ff33149f8087b25f35845e9
gayoung choi <cy614@naver.com>
2025-08-23 22:04:57 +0900

    feat : community controller에 ResponseEntity 추가

--- src/main/java/com/jeonsitalk/community/controller/CommController.java
+++ src/main/java/com/jeonsitalk/community/controller/CommController.java
@@ -3,6 +3,7 @@ package com.jeonsitalk.community.controller;
 import com.jeonsitalk.community.entity.Community;
 import com.jeonsitalk.community.service.CommService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,36 +17,40 @@ public class CommController {
     private final CommService commService;
 
     //service 계층을 추가하여 분리
+    //ResponseEntity를 사용하여 리팩토링, talend API로 테스트 완료
     //커뮤니티 글 전체 조회
     @GetMapping("/lists")
-    public List<Community> communityList() {
-        return commService.communityList();
+    public ResponseEntity<List<Community>> communityList() {
+        List<Community> lists =  commService.communityList();
+        return ResponseEntity.ok().body(lists); //게시글이 있는경우 200 ok와 body에 값 반환
     }
 
     //글 1개 조회(상세)
     @GetMapping("/lists/{id}")
-    public Community communityDetail(@PathVariable("id") Long id) {
+    public ResponseEntity<Community> communityDetail(@PathVariable("id") Long id) {
         Optional<Community> lists = commService.communityDetail(id);
-        return lists.orElse(null);
+        return lists.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()); //찾은 경우 200ok, 못 찾은 경우 404 not found
     }
 
     //글 작성
     @PostMapping("/writes")
-    public Community communityWrite(@RequestBody Community community) {
-        return commService.communityWrite(community);
+    public ResponseEntity<Community> communityWrite(@RequestBody Community community) {
+        Community write = commService.communityWrite(community);
+        return ResponseEntity.ok().body(write); //201 created()도 고려
     }
 
     //글 수정
     @PutMapping("/lists/{id}")
-    public Community communityUpdate(@RequestBody Community community, @PathVariable("id") Long id) {
+    public ResponseEntity<Community> communityUpdate(@RequestBody Community community, @PathVariable("id") Long id) {
         Optional<Community> modify = commService.communityModify(id, community);
-        return modify.orElseThrow();
+        return modify.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     //글 삭제
     @DeleteMapping("/lists/{id}")
-    public boolean communityDelete(@PathVariable("id") Long id) {
-        return commService.communityDelete(id);
+    public ResponseEntity<Community> communityDelete(@PathVariable("id") Long id) {
+            boolean delete = commService.communityDelete(id);
+            return  delete ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
 }
